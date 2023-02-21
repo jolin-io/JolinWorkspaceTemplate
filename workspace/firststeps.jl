af04f94939f80a23825c52b390489ca9c282de09
Stephan Sahm <stephan.sahm@jolin.io>
2023-02-21 11:11:12 +0100

    adding a second notebook

---
+++ workspace/firststeps.jl
@@ -0,0 +1,17 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d7795a36-b1cd-11ed-12e8-210a8bca4b85
+md"""
+# First steps with Jolin Workspace.
+"""
+
+# ╔═╡ 3133be06-db22-4434-96b9-e99bdea7ba3e
+1 + 1
+
+# ╔═╡ Cell order:
+# ╠═d7795a36-b1cd-11ed-12e8-210a8bca4b85
+# ╠═3133be06-db22-4434-96b9-e99bdea7ba3e
