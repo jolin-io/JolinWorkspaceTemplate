3edb46c60b0e488af62cb84bd8d334ee9c1bec6a
Stephan Sahm <stephan.sahm@jolin.io>
2023-03-16 15:57:08 +0000

    Update request from Jolin environment 'devpod'
    let's give it a try

--- workflows/welcome.jl
+++ workflows/welcome.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.22
+# v0.19.24
 
 using Markdown
 using InteractiveUtils
@@ -12,6 +12,10 @@ md"""
 # ╔═╡ 3133be06-db22-4434-96b9-e99bdea7ba3e
 1 + 1
 
+# ╔═╡ 70e61144-76ad-448d-87a9-1a82f3c1be2b
+error("fail")
+
 # ╔═╡ Cell order:
 # ╠═d7795a36-b1cd-11ed-12e8-210a8bca4b85
 # ╠═3133be06-db22-4434-96b9-e99bdea7ba3e
+# ╠═70e61144-76ad-448d-87a9-1a82f3c1be2b
