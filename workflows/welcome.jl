8efbae30b351108f70a6311bae4d7294c0d96a75
Stephan Sahm <stephan.sahm@jolin.io>
2023-03-16 15:35:33 +0000

    request
    some extra
    
    ---
    
    
    
    ---
    
    extra
    
    ---
    
    a new body
    
    ---
    
    include new changes
    
    ---
    
    adsads
    
    ---
    
    update

--- workflows/welcome.jl
+++ workflows/welcome.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.22
+# v0.19.24
 
 using Markdown
 using InteractiveUtils
@@ -12,6 +12,18 @@ md"""
 # ╔═╡ 3133be06-db22-4434-96b9-e99bdea7ba3e
 1 + 1
 
+# ╔═╡ 9c618c52-3e21-4466-8fa0-d468b00a64b7
+error("fail")
+
+# ╔═╡ 5f2d18b6-b30f-40f4-80eb-691750cdb4fa
+md"""
+# Header
+
+some text. and this
+"""
+
 # ╔═╡ Cell order:
 # ╠═d7795a36-b1cd-11ed-12e8-210a8bca4b85
 # ╠═3133be06-db22-4434-96b9-e99bdea7ba3e
+# ╠═9c618c52-3e21-4466-8fa0-d468b00a64b7
+# ╠═5f2d18b6-b30f-40f4-80eb-691750cdb4fa
