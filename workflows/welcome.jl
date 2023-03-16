fe6ff5e33814fa686d41078285be5c29368c5440
Stephan Sahm <stephan.sahm@jolin.io>
2023-03-16 13:41:58 +0000

    default message

--- workflows/welcome.jl
+++ workflows/welcome.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.22
+# v0.19.24
 
 using Markdown
 using InteractiveUtils
@@ -10,7 +10,7 @@ md"""
 """
 
 # ╔═╡ 3133be06-db22-4434-96b9-e99bdea7ba3e
-1 + 1
+10 + 10
 
 # ╔═╡ Cell order:
 # ╠═d7795a36-b1cd-11ed-12e8-210a8bca4b85
