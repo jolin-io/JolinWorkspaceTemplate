2927ce40bd88299092d4520301410e1758397b38
Stephan Sahm <stephan.sahm@jolin.io>
2023-03-17 07:17:22 +0000

    Update request from Jolin environment 'test'
    lets go

--- workflows/welcome.jl
+++ workflows/welcome.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.22
+# v0.19.25
 
 using Markdown
 using InteractiveUtils
@@ -12,6 +12,10 @@ md"""
 # ╔═╡ 3133be06-db22-4434-96b9-e99bdea7ba3e
 1 + 1
 
+# ╔═╡ 880ca2dc-811b-476a-8bd4-7ae36400c80e
+error("hello")
+
 # ╔═╡ Cell order:
 # ╠═d7795a36-b1cd-11ed-12e8-210a8bca4b85
 # ╠═3133be06-db22-4434-96b9-e99bdea7ba3e
+# ╠═880ca2dc-811b-476a-8bd4-7ae36400c80e
