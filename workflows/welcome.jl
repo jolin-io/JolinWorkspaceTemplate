294d61677134b68e44b4eaaa987c97a22f4f77c7
Stephan Sahm <stephan.sahm@jolin.io>
2023-04-27 14:23:18 +0000

    successfully connected to AWS

--- workflows/welcome.jl
+++ workflows/welcome.jl
@@ -20,11 +20,59 @@ using PlutoUI
 # ╔═╡ 6b788741-5288-46fe-81bd-7d4d26a82755
 using HTTP
 
+# ╔═╡ d9c231ad-777d-408a-865c-1ad6d199ce74
+using AWS: AWS, @service
+
 # ╔═╡ 761dab2c-863b-4bad-9b0d-032ae348c23a
 using JWTs
 
 # ╔═╡ 106fffed-66bb-43c6-97e3-579ce436efe6
 using HypertextLiteral, JSON3
+
+# ╔═╡ 670ad3cb-f401-4feb-8aee-ecbcbfe14595
+begin
+	using Dates: Dates, @dateformat_str
+	using AWS: AWSServices
+	"""
+	    credentials_from_webtoken()
+	
+	Assume role via web identity.
+	https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-role.html#cli-configure-role-oidc
+	"""
+	function jolin_webtoken_authorize_aws(role_arn; audience="", role_session::Union{AbstractString,Nothing}=nothing)
+		if isnothing(role_session)
+			role_session = AWS._role_session_name(
+	            "jolincloud-role-",
+	            basename(role_arn),
+	            "-" * Dates.format(Dates.now(Dates.UTC), dateformat"yyyymmdd\THHMMSS\Z"),
+	        )
+		end
+	    web_identity = @get_jwt(audience)
+	    
+	    response = AWSServices.sts(
+	        "AssumeRoleWithWebIdentity",
+	        Dict(
+	            "RoleArn" => role_arn,
+	            "RoleSessionName" => role_session,  # Required by AssumeRoleWithWebIdentity
+	            "WebIdentityToken" => web_identity,
+	        );
+	        aws_config=AWS.AWSConfig(; creds=nothing),
+	        feature_set=AWS.FeatureSet(; use_response_type=true),
+	    )
+	    dict = parse(response)
+	    role_creds = dict["AssumeRoleWithWebIdentityResult"]["Credentials"]
+	    assumed_role_user = dict["AssumeRoleWithWebIdentityResult"]["AssumedRoleUser"]
+	
+	    return AWS.global_aws_config(creds=AWS.AWSCredentials(
+	        role_creds["AccessKeyId"],
+	        role_creds["SecretAccessKey"],
+	        role_creds["SessionToken"],
+	        assumed_role_user["Arn"];
+	        expiry=Dates.DateTime(rstrip(role_creds["Expiration"], 'Z')),
+	        renew=() -> credentials_from_webtoken(role_arn; audience, role_session),
+		))
+	end
+end
 
 # ╔═╡ d7795a36-b1cd-11ed-12e8-210a8bca4b85
 md"""
@@ -54,6 +102,19 @@ print(serviceaccount_token)
 # ╔═╡ 45a1df85-b615-4110-89fe-eacbf4a162a3
 ENV
 
+# ╔═╡ 2d7b7588-de02-4bf9-b1bc-cb740c943640
+@service S3
+
+# ╔═╡ 6e806b11-f3b2-478e-b6c0-8fd231a9f966
+begin
+	ENV["AWS_DEFAULT_REGION"] = "eu-central-1"
+	role_arn = "arn:aws:iam::656386802830:role/test-role-to-assume-from-jolin"
+	jolin_webtoken_authorize_aws(role_arn, audience="awsaudience")
+end
+
+# ╔═╡ 14bbc073-9264-4295-bf64-afb078b24da5
+S3.get_object("jolin.io-testbucket", "test.txt")
+
 # ╔═╡ 87afcbe9-296c-4411-8072-a1c0c96e15d6
 HTTP.get("http://jolin-workspace-server-jwts.default/request_jwt",
 	query=["serviceaccount_token" => serviceaccount_token, "workflowpath" => "dummy", "audience" => "aws"])
@@ -69,10 +130,13 @@ macro get_jwt(audience="")
 		response = HTTP.get("http://jolin-workspace-server-jwts.default/request_jwt",
 			query=["serviceaccount_token" => $serviceaccount_token,
 				   "workflowpath" => $workflowpath,
-				   "audience" => $audience])
+				   "audience" => $(esc(audience))])
 		JSON3.read(response.body).token
 	end
 end
+
+# ╔═╡ 6e5856d3-dd34-4e1a-a026-385aaa05d1fe
+ print(@get_jwt("awsaudience"))
 
 # ╔═╡ ce4f0595-3341-4b89-b1b9-da1388696c18
 token_response = @get_jwt()
@@ -127,6 +191,8 @@ swap_output()
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
+AWS = "fbe9abb3-538b-5e4e-ba9e-bc94f4f92ebc"
+Dates = "ade2ca70-3891-5945-98fb-dc099432e06a"
 HTTP = "cd3eb016-35fb-5094-929b-558a96fad6f3"
 HypertextLiteral = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
 JSON3 = "0f8b85d8-7281-11e9-16c2-39a750bddbf1"
@@ -134,6 +200,7 @@ JWTs = "d850fbd6-035d-5a70-a269-1ca2e636ac6c"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
 
 [compat]
+AWS = "~1.84.1"
 HTTP = "~1.7.4"
 HypertextLiteral = "~0.9.4"
 JSON3 = "~1.12.0"
@@ -147,7 +214,13 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.8.5"
 manifest_format = "2.0"
-project_hash = "d6db6a6268b477ba898da05b6bc82b50443f9550"
+project_hash = "7b7bea4c3b3c0f0e0efda44e0f17b9793597186a"
+
+[[deps.AWS]]
+deps = ["Base64", "Compat", "Dates", "Downloads", "GitHub", "HTTP", "IniFile", "JSON", "MbedTLS", "Mocking", "OrderedCollections", "Random", "SHA", "Sockets", "URIs", "UUIDs", "XMLDict"]
+git-tree-sha1 = "c5a09e8e9b20b6f9e69c8ce83128798dba49d0b2"
+uuid = "fbe9abb3-538b-5e4e-ba9e-bc94f4f92ebc"
+version = "1.84.1"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -182,6 +255,12 @@ git-tree-sha1 = "eb7f0f8307f71fac7c606984ea5fb2817275d6e4"
 uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
 version = "0.11.4"
 
+[[deps.Compat]]
+deps = ["Dates", "LinearAlgebra", "UUIDs"]
+git-tree-sha1 = "7a60c856b9fa189eb34f5f8a6f6b5529b7942957"
+uuid = "34da2185-b29b-5c13-b0c7-acf172513d20"
+version = "4.6.1"
+
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
@@ -196,6 +275,17 @@ deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
 version = "1.6.0"
 
+[[deps.ExprTools]]
+git-tree-sha1 = "c1d06d129da9f55715c6c212866f5b1bddc5fa00"
+uuid = "e2ba6199-217a-4e67-a87a-7c52f15ade04"
+version = "0.1.9"
+
+[[deps.EzXML]]
+deps = ["Printf", "XML2_jll"]
+git-tree-sha1 = "0fa3b52a04a4e210aeb1626def9c90df3ae65268"
+uuid = "8f5d6c58-4d21-5cfd-889c-e3ad7ee6a615"
+version = "1.1.0"
+
 [[deps.FileWatching]]
 uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
 
@@ -204,6 +294,12 @@ deps = ["Statistics"]
 git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
 uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
 version = "0.8.4"
+
+[[deps.GitHub]]
+deps = ["Base64", "Dates", "HTTP", "JSON", "MbedTLS", "Sockets", "SodiumSeal", "URIs"]
+git-tree-sha1 = "5688002de970b9eee14b7af7bbbd1fdac10c9bbe"
+uuid = "bc5e4493-9b4d-5f90-b8aa-2b2bcaad7a26"
+version = "5.8.2"
 
 [[deps.HTTP]]
 deps = ["Base64", "CodecZlib", "Dates", "IniFile", "Logging", "LoggingExtras", "MbedTLS", "NetworkOptions", "OpenSSL", "Random", "SimpleBufferStream", "Sockets", "URIs", "UUIDs"]
@@ -237,6 +333,11 @@ version = "0.5.1"
 [[deps.InteractiveUtils]]
 deps = ["Markdown"]
 uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.IterTools]]
+git-tree-sha1 = "fa6287a4469f5e048d763df38279ee729fbd44e5"
+uuid = "c8e1da08-722c-5040-9ed9-7db0dc04731e"
+version = "1.4.0"
 
 [[deps.JLLWrappers]]
 deps = ["Preferences"]
@@ -284,6 +385,12 @@ version = "1.10.2+0"
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
 
+[[deps.Libiconv_jll]]
+deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
+git-tree-sha1 = "c7cb1f5d892775ba13767a87c7ada0b980ea0a71"
+uuid = "94ce4f54-9a6c-5748-9c1c-f9c7231a4531"
+version = "1.16.1+2"
+
 [[deps.LinearAlgebra]]
 deps = ["Libdl", "libblastrampoline_jll"]
 uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
@@ -320,6 +427,12 @@ version = "2.28.0+0"
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
+[[deps.Mocking]]
+deps = ["Compat", "ExprTools"]
+git-tree-sha1 = "782e258e80d68a73d8c916e55f8ced1de00c2cea"
+uuid = "78c3b35d-d492-501b-9361-3d52fe80e533"
+version = "0.7.6"
+
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
 version = "2022.2.1"
@@ -344,6 +457,11 @@ deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
 git-tree-sha1 = "9ff31d101d987eb9d66bd8b176ac7c277beccd09"
 uuid = "458c3c95-2e84-50aa-8efc-19380b2a3a95"
 version = "1.1.20+0"
+
+[[deps.OrderedCollections]]
+git-tree-sha1 = "d321bf2de576bf25ec4d3e4360faca399afca282"
+uuid = "bac558e1-5e72-5ebc-8fee-abe8a469f55d"
+version = "1.6.0"
 
 [[deps.Parsers]]
 deps = ["Dates", "SnoopPrecompile"]
@@ -406,6 +524,12 @@ version = "1.0.3"
 [[deps.Sockets]]
 uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
 
+[[deps.SodiumSeal]]
+deps = ["Base64", "Libdl", "libsodium_jll"]
+git-tree-sha1 = "80cef67d2953e33935b41c6ab0a178b9987b1c99"
+uuid = "2133526b-2bfb-4018-ac12-889fb3908a75"
+version = "0.1.1"
+
 [[deps.SparseArrays]]
 deps = ["LinearAlgebra", "Random"]
 uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
@@ -457,6 +581,18 @@ uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
 [[deps.Unicode]]
 uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 
+[[deps.XML2_jll]]
+deps = ["Artifacts", "JLLWrappers", "Libdl", "Libiconv_jll", "Pkg", "Zlib_jll"]
+git-tree-sha1 = "93c41695bc1c08c46c5899f4fe06d6ead504bb73"
+uuid = "02c8fc9c-b97f-50b9-bbe4-9be30ff0a78a"
+version = "2.10.3+0"
+
+[[deps.XMLDict]]
+deps = ["EzXML", "IterTools", "OrderedCollections"]
+git-tree-sha1 = "d9a3faf078210e477b291c79117676fca54da9dd"
+uuid = "228000da-037f-5747-90a9-8195ccbf91a5"
+version = "0.4.1"
+
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
@@ -466,6 +602,12 @@ version = "1.2.12+3"
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
 version = "5.1.1+0"
+
+[[deps.libsodium_jll]]
+deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
+git-tree-sha1 = "848ab3d00fe39d6fbc2a8641048f8f272af1c51e"
+uuid = "a9144af2-ca23-56d9-984f-0d03f7b5ccf8"
+version = "1.0.20+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -489,6 +631,12 @@ version = "17.4.0+0"
 # ╠═0b134780-f11c-4167-a5e3-87e2834eb661
 # ╠═2190d645-bf49-434d-9f8b-0622d182995a
 # ╠═45a1df85-b615-4110-89fe-eacbf4a162a3
+# ╠═670ad3cb-f401-4feb-8aee-ecbcbfe14595
+# ╠═d9c231ad-777d-408a-865c-1ad6d199ce74
+# ╠═2d7b7588-de02-4bf9-b1bc-cb740c943640
+# ╠═6e806b11-f3b2-478e-b6c0-8fd231a9f966
+# ╠═14bbc073-9264-4295-bf64-afb078b24da5
+# ╠═6e5856d3-dd34-4e1a-a026-385aaa05d1fe
 # ╠═87afcbe9-296c-4411-8072-a1c0c96e15d6
 # ╠═b2f3f419-1b66-4901-b60e-78714f3b9cac
 # ╠═ce4f0595-3341-4b89-b1b9-da1388696c18
