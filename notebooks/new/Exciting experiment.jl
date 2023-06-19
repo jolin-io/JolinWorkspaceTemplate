93fa23dec78d458ad10dc08fa778e088d7461eda
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-19 08:31:56 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -60,7 +60,7 @@ end
 # ╔═╡ 1b63f8ed-010c-4eb5-af37-a88204989947
 for name in readdir("../../../.julia/artifacts")
 	dir = "../../../.julia/artifacts/" * name
-	isdir(dir * "/lib") || @show readdir(dir); continue
+	isdir(dir * "/lib") || (@show readdir(dir); continue)
 	@show readdir(dir * "/lib")
 end
 
