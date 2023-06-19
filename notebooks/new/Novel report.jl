3c1f1e6c09193e6d98a95f9dc783ea71d18c2268
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-19 14:07:14 +0000

    automatic backup

--- notebooks/new/Novel report.jl
+++ notebooks/new/Novel report.jl
@@ -66,9 +66,6 @@ end
 # ╔═╡ ee5e7303-1aaa-4ae7-9518-dbfdd5687971
 myvalue2 = @newtake_repeatedly! channel2
 
-# ╔═╡ d8b58774-d094-47ec-b7a1-c89a1f2aebc3
-@newtake_repeatedly! channel2
-
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
@@ -596,6 +593,5 @@ version = "17.4.0+0"
 # ╠═16ed0226-94d1-40f4-9085-288c89e356a8
 # ╠═094c6db9-e6ab-41ec-b114-d3cfd7eef8d7
 # ╠═ee5e7303-1aaa-4ae7-9518-dbfdd5687971
-# ╠═d8b58774-d094-47ec-b7a1-c89a1f2aebc3
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
