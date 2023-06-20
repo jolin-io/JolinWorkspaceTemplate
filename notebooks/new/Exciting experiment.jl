ba978a1a4c88d5ef6456f974bd9a7b51967c4a59
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-20 15:03:03 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -131,7 +131,6 @@ result = inference(
 	model = kalman_filter(prior_x_mean[], prior_x_var[], prior_τ_shape[], prior_τ_rate[]),
 	data = (y = regular_price,),
 	constraints = filter_constraints(),
-	returnvars = (x_current = KeepLast(), ),
     free_energy = true
 )
 
