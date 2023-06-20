fea5df457952eaa2ebc40c1a0718d23c8c739902
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-20 15:16:12 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -134,7 +134,7 @@ result = inference(
 )
 
 # ╔═╡ 724e47b4-7277-4250-b72a-3e4649ac6261
-result.posteriors
+result.posteriors.τ
 
 # ╔═╡ 140691bc-765d-448d-ac33-90cc9b86b1b4
 fieldnames(typeof(result))
