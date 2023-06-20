409f1feeb9018450ea14a7b53452b7df60e669e7
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-20 14:53:53 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -94,6 +94,11 @@ end
 # ╔═╡ 14d9736c-9daf-4ac6-a24a-cab83bb350f6
 begin
 	@model function kalman_filter(x_prev_mean, x_prev_var)
+
+		# Prior for the previous state
+	    x_prev_mean = datavar(Float64)
+	    x_prev_var  = datavar(Float64)
+			
 	    x_prev ~ Normal(mean = x_prev_mean, variance = x_prev_var)
 	    
 	    # Prior for the observation noise
