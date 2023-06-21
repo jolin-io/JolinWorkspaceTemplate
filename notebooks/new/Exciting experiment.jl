ad1b61ad74a8eaa948e8cfb19f6c8c1b8fefd55b
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-21 11:55:31 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -40,6 +40,9 @@ begin
 	"""
 end
 
+# ╔═╡ f858490e-541c-4668-921b-71aef9db5710
+
+
 # ╔═╡ 25e8b93b-3026-40b7-b41f-016e059b838d
 BINANCE_API_WS = "wss://stream.binance.com:9443/ws/$trading@miniTicker"
 
@@ -1919,6 +1922,7 @@ version = "1.4.1+0"
 # ╠═89a620ac-8649-4e46-9abd-dfbc4ef00f32
 # ╠═0d8791d8-ff47-495f-ba74-f1f565654d66
 # ╠═8987a81f-6148-43ad-a6d0-f7adb425c8e3
+# ╠═f858490e-541c-4668-921b-71aef9db5710
 # ╠═4d6d2ae9-6456-401a-980b-9215d35e32c2
 # ╠═25e8b93b-3026-40b7-b41f-016e059b838d
 # ╠═a1e6e7c2-ba41-484c-bb3c-20bc8c1c63c6
