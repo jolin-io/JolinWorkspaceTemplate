fe25861abc7fff61b09878ae40293d8c127a955e
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-21 07:27:55 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -186,7 +186,7 @@ begin
 end
 
 # ╔═╡ 639ea99f-8787-4212-99d8-4002763d8493
-mean(posteriors[end][:y_τ])
+sample(posteriors[end][:y_τ])
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
