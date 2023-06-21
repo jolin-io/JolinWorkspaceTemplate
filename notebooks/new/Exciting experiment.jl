fdc5de63ebc6e2f9f0c7fef8bf6d86e48fd92503
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-21 07:33:19 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -195,7 +195,7 @@ rand(Gamma(shape(posteriors[end][:y_τ]), scale(posteriors[end][:y_τ])))
 posteriors[end][:y_τ]
 
 # ╔═╡ 639ea99f-8787-4212-99d8-4002763d8493
-rand(posteriors[end][:y_τ, (2,3)])
+rand(posteriors[end][:y_τ], (2,3))
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
