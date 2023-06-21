6787729ad96d28a8e4a515f45dbb75d3c34a9968
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-21 11:16:28 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -219,7 +219,7 @@ end
 # ╔═╡ 03aa263a-7b1a-453e-b860-fa36296f816d
 begin
 	x_mean, x_std = mean_std(posteriors[end][:x])
-	y_mean, y_std = y_means[end], y_cis[end]
+	y_mean, y_ci = y_means[end], y_cis[end]
 	
 	x_mean = Int(round(x_mean))
 	x_std = Int(round(x_std))
