21269e2350cd5fdb6d6c76c5397d5d56e4a4fa19
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-21 14:05:28 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -187,7 +187,7 @@ end
 @bind a Slider(1:40) 
 
 # ╔═╡ a2401fbe-0f32-46d7-87b8-6c544e369345
-
+@bind b NumberField(0:100)
 
 # ╔═╡ 547383f5-ddc6-4698-8532-f434af16fff5
 x_tau_mean, x_tau_std = mean_std(GammaShapeRate(40, 70))
