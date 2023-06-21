1fe3ad69d542a79ab1ae5f6ed636bd00839e9869
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-21 09:40:40 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -220,6 +220,7 @@ for p in posteriors
 	m, v = mean_var(p[:y_τ])
 	lb, ub = 1/(m-v), 1/(m+v)
 	println("$lb .. $ub)
+end
 
 # ╔═╡ 41f94e92-2346-4e75-9c2c-85eff46274b0
 rand(Gamma(shape(posteriors[end][:y_τ]), scale(posteriors[end][:y_τ])))
