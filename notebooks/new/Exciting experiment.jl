938fb30777cc0d33ff53c26cf3c678d8dcd53de3
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-21 10:18:19 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -226,10 +226,10 @@ begin
 	y_std = Int(round(y_std))
 	
 	md"""
-	|  | current estimations |
+	| average in 69.2% confidence | current estimations |
 	|---------------------|:------------|
-	|hidden state (average in 69.2% confidence) | $(x_mean)€ ± $(x_std)€
-	|observed state (average in 69.2% confidence) | $(y_mean)€ ± $(y_std)€|
+	|hidden state | $(x_mean)€ ± $(x_std)€
+	|observed state | $(y_mean)€ ± $(y_std)€|
 	"""
 end
 
