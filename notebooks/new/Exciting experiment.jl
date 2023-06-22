69da4a32836d71058904b3507ca33a8daf492792
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-22 08:12:29 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -283,6 +283,9 @@ pushed_posterior = begin
 	push_sliding!(prob_prices, regular_price, n=regular_n)
 end;
 
+# ╔═╡ 3567ea3e-b3a0-4d85-8be3-935127fbbcb4
+
+
 # ╔═╡ 3b676410-ec35-4ead-8bb6-e2c9a172016a
 begin
 	pushed_posterior
@@ -2063,6 +2066,7 @@ version = "1.4.1+0"
 # ╠═20e443bc-86b2-4ac0-8ba6-f5bc7d1c46ff
 # ╠═7ad62d34-3775-48cd-aec1-1fbe6788be72
 # ╠═0ae048ec-9367-4d75-8b05-51404775e23f
+# ╠═3567ea3e-b3a0-4d85-8be3-935127fbbcb4
 # ╠═3b676410-ec35-4ead-8bb6-e2c9a172016a
 # ╠═a37c1579-ad9e-46db-91e8-a3d30acb0cf9
 # ╠═03aa263a-7b1a-453e-b860-fa36296f816d
