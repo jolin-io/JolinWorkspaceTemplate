f3b118ec5dc24247038e8425857c064180793d0b
Stephan Sahm <stephan.sahm@jolin.io>
2023-06-22 09:29:56 +0000

    automatic backup

--- notebooks/new/Exciting experiment.jl
+++ notebooks/new/Exciting experiment.jl
@@ -234,9 +234,6 @@ result = inference(
 # ╔═╡ d3dcebdf-7224-4ded-bfa4-e961ee4407e6
 result.posteriors
 
-# ╔═╡ 5dcecef4-9da2-4de6-8710-652f0c522581
-collect(1:1)
-
 # ╔═╡ 8d76aad4-d592-41d5-a38c-4aa4364a2045
 function repeatcall(f, args...; shape, kwargs...)
 	linear = [f(args...; kwargs...) for i in 1:prod(shape)]
@@ -2098,7 +2095,6 @@ version = "1.4.1+0"
 # ╠═14d9736c-9daf-4ac6-a24a-cab83bb350f6
 # ╠═b4d880a6-992e-4e30-9837-3f1cf8f4eb8d
 # ╠═d3dcebdf-7224-4ded-bfa4-e961ee4407e6
-# ╠═5dcecef4-9da2-4de6-8710-652f0c522581
 # ╠═8d76aad4-d592-41d5-a38c-4aa4364a2045
 # ╠═670da837-f557-4a78-a19d-4df835a46d00
 # ╠═cc6d218d-d66f-45b8-9dfb-c54f0d7eaa8b
