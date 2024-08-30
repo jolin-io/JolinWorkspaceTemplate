91fdbaebfeba07a864da0243d319a1aeb78be8be
Stephan Sahm <stephan.sahm@jolin.io>
2024-08-30 11:11:18 +0000

    automatic backup

---
+++ notebooks/new/Friendly science.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 61196d14-66c0-11ef-35da-a5ee5d8633ff
+using Plots
+
+# ╔═╡ Cell order:
+# ╠═61196d14-66c0-11ef-35da-a5ee5d8633ff
